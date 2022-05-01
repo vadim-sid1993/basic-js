7ecc61b47636681582353952082ab222b99811c0
vadim-sid1993 <79473969+vadim-sid1993@users.noreply.github.com>
2022-05-01 17:22:34 +0300

    created drea-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -16,6 +16,29 @@ const { NotImplementedError } = require('../extensions/index.js');
 function createDreamTeam(/* members */) {
   throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let x = [];
+  let y = [];
+  if (members.isArray) {
+    for (i = 0; i < members.length; i++) {
+      if (typeof (members[i]) == "string") {
+        x.push(members[i])
+      }
+    }
+    for (i = 0; i < x.length; i++) {
+      if (x[i] == ' ') {
+        x.splice(i, 1)
+      }
+    }
+    for (i = 0; i < x.length; i++) {
+      let z = x[i].split('')
+      y.push(z[0])
+    }
+    y.sort();
+    let nameTeam = y.join('');
+    return nameTeam
+  } else {
+    return false
+  }
 }
 
 module.exports = {

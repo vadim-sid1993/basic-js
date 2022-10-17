03657946148740a4bcd99260240111c94d62acd4
vadim-sid1993 <79473969+vadim-sid1993@users.noreply.github.com>
2022-10-17 12:07:13 +0300

    init: add count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,13 +14,13 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+function countCats(matrix ) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
   let numberCat = 0;
     for (i=0; i<matrix.length; i++) {
         for (j=0; j<matrix[i].length; j++) {
-            if (j==='^^'){
+            if (matrix[i][j] === '^^'){
                 numberCat +=1;
             }
         }
